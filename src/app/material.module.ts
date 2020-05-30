28ab0f9309261dd1d9e8718e2f6c2be7a6824bc1
Ban <paulfabre2009@hotmail.fr>
2020-05-30 18:11:36 +0200

    ajout barre de nav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
